b037bf53777694e85c0f9b504e5f873e15c93b4e
Bilal Basha M <bilal.basham@nielseniq.com>
2023-12-10 23:51:03 +0530

    Sentry added

--- src/index.js
+++ src/index.js
@@ -6,8 +6,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
+import * as Sentry from '@sentry/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+Sentry.init({
+  dsn: "https://20a67cc4c45d3d1162722150b52f7979@o4506372799135744.ingest.sentry.io/4506372802478080",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
